e8b6160761790882bcd805bc33abf3a369aeb6b8
Jesse Horner <jrhorner@pm.me>
2020-12-06 12:17:21 -0500

    Day 3 improved

--- day3/slope.go
+++ day3/slope.go
@@ -2,57 +2,22 @@ package main
 
 import (
 	"fmt"
-	"../utils"
+	"io/ioutil"
+	"image"
+	"strings"
 )
 
-func checkSlope(mov []int, input []string) int {
-	trees := 0
-	pos := []int{0, 0} // x, y
-	for y, i := range input {
-		if y != pos[1] {
-			continue
-		}
-		if string(i[pos[0]]) == "#" {
-			trees++
-		}
-		if pos[0] + mov[0] >= len(i) {
-			pos[0] += mov[0] - len(i)
-		} else {
-			pos[0] += mov[0]
-		}
-		pos[1] += mov[1]
-	}
-	return trees
-}
-
-func p1(input []string) {
-	slope := []int{3, 1}
-	trees := checkSlope(slope, input)
-	fmt.Println("Part 1:", trees)
-}
-
-func p2(input []string) {
-	slopes := [][]int{
-		[]int{1, 1},
-		[]int{3, 1},
-		[]int{5, 1},
-		[]int{7, 1},
-		[]int{1, 2}}
-	trees := []int{0, 0, 0, 0, 0}
-	product := 0
-	for i, slope := range slopes {
-		trees[i] = checkSlope(slope, input)
-		if i == 0 {
-			product = trees[i]
-		} else {
-			product = product * trees[i]
-		}
-	}
-	fmt.Println("Part 2:", product)
-}
-
 func main() {
-	input := utils.OpenFile()
-	p1(input)
-	p2(input)
+	input, _ := ioutil.ReadFile("input")
+	grid := strings.Fields(string(input))
+	slopes := map[image.Point]int{ {1, 1}: 0, {3, 1}: 0, {5, 1}: 0, {7, 1}: 0, {1, 2}: 0 }
+	product := 1
+	for s := range slopes {
+		for p := (image.Point{}); p.Y < len(grid); p = p.Add(s){
+			if grid[p.Y][p.X % len(grid[0])] == '#' { slopes[s]++ }
+		}
+		product *= slopes[s]
+	}
+	fmt.Println("Part 1:", slopes[image.Point{3, 1}])
+	fmt.Println("Part 2:", product)
 }
