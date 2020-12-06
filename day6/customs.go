1bf626a73f56b72964625acd3b53a507a697626d
Jesse Horner <jrhorner@pm.me>
2020-12-06 00:47:14 -0500

    Part 1 simplified

--- day6/customs.go
+++ day6/customs.go
@@ -9,10 +9,10 @@ import (
 
 func p1(answers []string) {
 	count := 0
-	q := [26]string{ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	q := "abcdefghijklmnopqrstuvwxyz"
 	for _, i := range answers {
 		for _, j := range q {
-			match, _ := regexp.MatchString(j, i)
+			match, _ := regexp.MatchString(string(j), i)
 			if match { count++ }
 		}
 	}
