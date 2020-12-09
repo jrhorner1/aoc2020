0c04b4608a778037a09e5ec6bd239f87e30bff93
Jesse Horner <jrhorner@pm.me>
2020-12-09 14:44:54 -0500

    updated template

--- template.go
+++ template.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
 func main() {
 	input, _ := ioutil.ReadFile("input")
+	in := strings.Split(strings.TrimSpace(string(input)), "\n")
 	// fmt.Println("Part 1:", input)
 	// fmt.Println("Part 2:", input)
 }
